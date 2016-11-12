536617549d4b30726f7d6640d4b3ab32d5ed808e
Jack Loughran <loughranjack28@gmail.com>
2016-11-12 14:06:18 -0500

    attempt to fix mobile submit for email

--- js/index.js
+++ js/index.js
@@ -12,4 +12,12 @@ ready(function() {
     booking.style.display = 'none';
     document.querySelector('#email').style.display = 'block';
   }, false);
+
+  const newsfeed = document.querySelector('#mce-EMAIL');
+  newsfeed.addEventListener('keypress', function(e) {
+    if (e.keyCode === 13) {
+      const submit = document.querySelector('#submit');
+      submit.click();
+    }
+  });
 });
